d64c00018e4cf2592844871af4423e5cbc61d407
Markus Klein <markus-klein@live.de>
2023-05-05 22:09:36 +0200

    alternative negamax implementation

--- src/solver.rs
+++ src/solver.rs
@@ -1,8 +1,8 @@
 use crate::{Column, ConnectFour};
 
-/// Calculates the score of a connect four game. ~~The score is set up so always picking the move with
-/// the highest score results in perfect play. Perfect meaning winning as fast as possible, drawing
-/// or loosing as late as possible.~~
+/// Calculates the score of a connect four game. The score is set up so always picking the move with
+/// the lowest score results in perfect play. Perfect meaning winning as fast as possible, drawing
+/// or loosing as late as possible.
 ///
 /// A positive score means the player who can put in the next stone can win. Positions which can be
 /// one faster are scored higher. The score is 1 if the current player can win with his last stone.
@@ -26,12 +26,31 @@ pub fn score(game: &ConnectFour) -> i32 {
         return (42 + 1 - game.stones() as i32) / 2;
     }
 
-    // Opponent gets to choose the best move for him
-    let best_score_for_opponent = (0..7).filter_map(|col| {
+    // We play the position which is the worst for our opponent
+    let worst_score_for_opponent = (0..7).filter_map(|col| {
         let mut next = *game;
         next.play_move(&Column::from_index(col)).then(|| score(&next))
     }).min().expect("There must be at least one legal move");
 
     // Score from the perspective of the current player is the negative of the opponents.
-    -best_score_for_opponent
+    -worst_score_for_opponent
+}
+
+pub fn score_slow(game: &ConnectFour) -> i32 {
+    if game.is_victory() {
+        let player_stones_left = (42 - game.stones() as i32) / 2;
+        return -(player_stones_left + 1);
+    }
+    // Draw game
+    if game.stones() == 42 {
+        return 0;
+    }
+
+    let worst_score_for_opponent = (0..7).filter_map(|col| {
+        let mut next = *game;
+        next.play_move(&Column::from_index(col)).then(|| score_slow(&next))
+    }).min().expect("There must be at least one legal move");
+
+    // Score from the perspective of the current player is the negative of the opponents.
+    -worst_score_for_opponent
 }
